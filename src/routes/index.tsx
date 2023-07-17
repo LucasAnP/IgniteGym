c76baf944bae4e42a3444abdec51ff3ba99ac1a1
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-07-17 17:29:29 -0300

    Navigation: Avoiding glitch

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from "native-base";
+import { useTheme, Box } from "native-base";
 import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
 
@@ -8,8 +8,10 @@ export function Routes() {
   theme.colors.background = colors.gray[700];
 
   return (
-    <NavigationContainer theme={theme}>
-      <AuthRoutes />
-    </NavigationContainer>
+    <Box flex={1} bg="gray.700">
+      <NavigationContainer theme={theme}>
+        <AuthRoutes />
+      </NavigationContainer>
+    </Box>
   );
 }
