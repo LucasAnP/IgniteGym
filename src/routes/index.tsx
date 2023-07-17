6042d38ac21ef27d280bf974389ee98e5a83dcf5
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-07-17 17:28:45 -0300

    Navigation: Changing colors os navigationContainer

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,14 @@
-import { NavigationContainer } from "@react-navigation/native";
+import { useTheme } from "native-base";
+import { NavigationContainer, DefaultTheme } from "@react-navigation/native";
 import { AuthRoutes } from "./auth.routes";
 
 export function Routes() {
+  const { colors } = useTheme();
+  const theme = DefaultTheme;
+  theme.colors.background = colors.gray[700];
+
   return (
-    <NavigationContainer>
+    <NavigationContainer theme={theme}>
       <AuthRoutes />
     </NavigationContainer>
   );
