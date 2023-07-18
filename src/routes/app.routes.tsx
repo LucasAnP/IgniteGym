020df935adfc407a51104cc546715ed0429128c8
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-07-18 13:06:11 -0300

    Private Routes - Given styles to bottom navigator

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -1,3 +1,4 @@
+import { Platform } from "react-native";
 import { useTheme } from "native-base";
 import {
   createBottomTabNavigator,
@@ -35,6 +36,13 @@ export function AppRoutes() {
         tabBarShowLabel: false,
         tabBarActiveTintColor: colors.green[500],
         tabBarInactiveTintColor: colors.gray[200],
+        tabBarStyle: {
+          backgroundColor: colors.gray[600],
+          borderTopWidth: 0,
+          height: Platform.OS === "android" ? "auto" : 96,
+          paddingBottom: sizes[10],
+          paddingTop: sizes[6],
+        },
       }}
     >
       <Screen
@@ -64,7 +72,13 @@ export function AppRoutes() {
           ),
         }}
       />
-      <Screen name="exercise" component={Exercise} />
+      <Screen
+        name="exercise"
+        component={Exercise}
+        options={{
+          tabBarButton: () => null,
+        }}
+      />
     </Navigator>
   );
 }
