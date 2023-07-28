61b46c11b3a51ede016eceef8bf3b0092c0ed72f
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-07-28 15:59:20 -0300

    Adjustments

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -152,7 +152,7 @@ export function SignUp() {
         <Button
           title="Back to Login"
           variant={"outline"}
-          mt={24}
+          mt={12}
           onPress={handleGoBack}
         />
       </VStack>
