a07f4bd01251b490f27b1415503e47d0916c1c8c
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-08-04 17:37:21 -0300

    Add requests in the queue

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,8 +1,13 @@
-import axios, { AxiosInstance } from "axios";
+import axios, { AxiosError, AxiosInstance } from "axios";
 import { AppError } from "@utils/AppError";
 import { storageAuthGetToken } from "@storage/storageAuthToken";
 
 type SignOut = () => void;
+
+type PromiseType = {
+  onSuccess: (token: string) => void;
+  onFailure: (error: AxiosError) => void;
+};
 
 type APIInstanceProps = AxiosInstance & {
   registerInterceptTokenManager: (signOut: SignOut) => () => void;
@@ -12,6 +17,9 @@ const api = axios.create({
   baseURL: "http://192.168.100.69:3333",
   timeout: 6000,
 }) as APIInstanceProps;
+
+let failedQueue: Array<PromiseType> = [];
+let isRefreshing = false;
 
 api.registerInterceptTokenManager = (signOut) => {
   const interceptTokenManager = api.interceptors.response.use(
@@ -28,6 +36,25 @@ api.registerInterceptTokenManager = (signOut) => {
             signOut();
             return Promise.reject(requestError);
           }
+
+          const originalRequestConfig = requestError.config;
+
+          if (isRefreshing) {
+            return new Promise((resolve, reject) => {
+              failedQueue.push({
+                onSuccess: (token: string) => {
+                  originalRequestConfig.headers = {
+                    authorization: `Bearer ${token}`,
+                  };
+                  resolve(api(originalRequestConfig));
+                },
+                onFailure: (error: AxiosError) => {
+                  reject(error);
+                },
+              });
+            });
+          }
+          isRefreshing = true;
         }
 
         signOut();
