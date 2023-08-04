fc690682bdcb9d3e9b37d99b16fbf9b391887e14
LucasAnSilvs <lucas.antonio@webjump.com.br>
2023-08-04 17:07:49 -0300

    Verifying if the token is invalid

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,10 +1,46 @@
-import axios from "axios";
+import axios, { AxiosInstance } from "axios";
 import { AppError } from "@utils/AppError";
+
+type SignOut = () => void;
+
+type APIInstanceProps = AxiosInstance & {
+  registerInterceptTokenManager: (signOut: SignOut) => () => void;
+};
 
 const api = axios.create({
   baseURL: "http://192.168.100.69:3333",
   timeout: 6000,
-});
+}) as APIInstanceProps;
+
+api.registerInterceptTokenManager = (signOut) => {
+  const interceptTokenManager = api.interceptors.response.use(
+    (response) => response,
+    (requestError) => {
+      if (requestError?.response?.status === 401) {
+        if (
+          requestError.response.data?.message === "token.expired" ||
+          requestError.response.data?.message === "token.invalid"
+        ) {
+        }
+
+        signOut();
+      }
+
+      // Validate is the error is handled
+      if (requestError.response && requestError.response.data) {
+        // Message handled by backend
+        return Promise.reject(new AppError(requestError.response.data.message));
+      } else {
+        // Message not handled by backend
+        return Promise.reject(requestError);
+      }
+    }
+  );
+
+  return () => {
+    api.interceptors.response.eject(interceptTokenManager);
+  };
+};
 
 api.interceptors.response.use(
   (response) => response,
